d8126fc24a799fb4be397ffae072880fed5b9738
Jillo Wario <willywario0@gmail.com>
2023-12-14 14:03:19 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(5,20,14,9,11)
